6af6a2c45624aec461a661a3ce6c5897333872ce
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2021-07-04 21:54:48 +0600

    Add some text style in index.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 window.addEventListener("scroll", function () {
   const nav = document.querySelector("nav");
+
   nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
 });
